62ae2e19cb453ff62a4e9e2e2aefd3cb3f618322
Zach Hall <zach@sosguy.net>
2010-02-10 23:38:09 -0800

    Added a group editor!
    One equal sign can make all the difference >_>

--- ext/permissions/main.php
+++ ext/permissions/main.php
@@ -15,7 +15,8 @@
  
 class Permissions extends SimpleExtension {
 	/**
-	 * This section handles the "master list" of permissions.
+	 * Functions of the Permissions Class:
+	 *		+ Managing the "master list" of permissions.
 	 */
 	
 	// onEvents go here.
@@ -32,7 +33,7 @@ class Permissions extends SimpleExtension {
 		 *
 		 * REMINDER: If I change the database tables, I must change up version by 1.
 		 */
-		 if($version < 1) {
+		 if($version < 2) {
 		 	/**
 		 	* Installer
 		 	*/
@@ -40,9 +41,9 @@ class Permissions extends SimpleExtension {
 			$database->create_table("permission_list",
                 "id SCORE_AIPK
 				 , perm_name VARCHAR(128) UNIQUE NOT NULL
-                 , INDEX(id)
+				 , perm_desc TEXT
                 ");
-			$config->set_int("permission_list", 3);
+			$config->set_int("permission_list", 2);
 		}
 		// Send the event and let all the extensions add their permissions.
 		send_event(new PermissionsSetEvent());
@@ -55,18 +56,20 @@ class PermissionsSetEvent extends Event {
 	 * Hm... If we "event-ize" this, it would eliminate the need to
 	 * create a bunch of globals junk. Let's try it ^_^
 	 */
-	public function add_perm($name) {
+	public function add_perm($name, $desc) {
 		/**
 		 * This simple function adds a permission to the list.
 		 *
 		 * If the permission already exists, don't do this.
 		 */
 		global $database;
+		if(!isset($name)) { die("No name permission error."); }
+		if(!isset($desc)) { $desc = "Unknown Permission."; }
 		$exists = $database->get_row("SELECT perm_name FROM permission_list WHERE perm_name = ?", array($name));
 
 		if($exists['perm_name'] != $name) {
 			// Looks good. Let's add it.
-			$database->execute("INSERT INTO `permission_list` (`id`, `perm_name`) VALUES ('', ?)", array($name));
+			$database->execute("INSERT INTO `permission_list` (`id`, `perm_name`, `perm_desc`) VALUES ('', ?, ?)", array($name, $desc));
 			log_info("permissions","Added permission $name.");
 		}
 	}
@@ -75,9 +78,13 @@ class PermissionsSetEvent extends Event {
 class Permissions_Test extends SimpleExtension {
 	public function onPermissionsSet(Event $event) {
 		// This test extension does what all extensions would do if this system is implemented.
-		// Right now, it just sets a permission.
+		// Right now, it just sets a few permissions that would be deemed necessary, but should be set elsewhere, such as in the extensions themselves.
 		// This function might make globals unnecessary.
-		$event->add_perm("Hello World");
+		$event->add_perm("can_post", "Post");
+		$event->add_perm("can_comment", "Comment");
+		$event->add_perm("can_delete_posts", "Delete Posts");
+		$event->add_perm("can_delete_comments", "Delete Comments");
+		$event->add_perm("can_bulk_upload", "Bulk Upload");
 	}
 }
 
@@ -109,29 +116,91 @@ class Permissions_Test extends SimpleExtension {
 //}
 
 class Groups extends SimpleExtension {
+	/**
+	 * Functions of the groups extension:
+	 *		+ Install the groups table (this class)
+	 *		+ Group Editor (GroupEditor class)
+	 *		+ Assigning permissions to a member of a group
+	 */
 	public function onInitExt(Event $event) {
 		/**
 		 * OK... we are going to install some tables.
 		 * And by some, I mean one (for now.)
 		 */
 		global $config;
-		$version = $config->get_int("permission_list", 0);
+		$version = $config->get_int("group_list", 0);
 		/**
 		 * If this version is less than "1", it's time to install.
 		 *
 		 * REMINDER: If I change the database tables, I must change up version by 1.
 		 */
-		 if($version < 1) {
+		 if($version < 2) {
 		 	/**
 		 	* Installer
 		 	*/
 			global $database;
-			$database->create_table("permission_list",
+			$database->create_table("group_list",
                 "id SCORE_AIPK
-				 , perm_name VARCHAR(128) UNIQUE NOT NULL
-                 , INDEX(id)
+				 , group_name VARCHAR(128) UNIQUE NOT NULL
+				 , group_permissions TEXT
+				 , group_members TEXT
                 ");
-			$config->set_int("permission_list", 3);
+			$config->set_int("group_list", 2);
+		}
+	}
+}
+
+class GroupEditor extends SimpleExtension {
+	public function onPageRequest(Event $event) {
+		if($event->page_matches("groups/editor")) {
+		/**
+		 * Displays the groups editor.
+		 */
+			global $user, $database;
+			if(!$user->is_admin()) {
+				$this->theme->display_permission_denied($page);
+			} else {
+				$permissions = $database->get_all("SELECT * FROM permission_list ORDER BY id ASC");
+				$groups = $database->get_all("SELECT * FROM group_list ORDER BY id ASC");
+				$this->theme->display_editor($permissions, $groups);
+			}
+		}
+		
+		if($event->page_matches("groups/add")) {
+			/**
+			 * Adds a group
+			 */
+			global $page, $database, $user;
+			if(!$user->is_admin()) {
+				$this->theme->display_permission_denied($page);
+			} else {
+				/**
+				 * Consolidating function to get all of the permissions in order.
+				 * Argh this will be tough...
+				 */
+				$group_name = $_POST['group_name'];
+				// Get all permissions
+				$permissions = $database->get_all("SELECT * FROM permission_list ORDER BY id ASC");
+				// Now, start a loop...
+				for ($i = 0 ; $i < count($permissions) ; $i++) {
+					$pn = $permissions[$i]['perm_name'];
+					if(isset($_POST["$pn"])) { $pe[] = $pn; }
+					//$pe[] = isset($_POST["$pn"])? "$pn" : ""; // will this work?
+				}
+				
+				for ($i = 0 ; $i < count($pe) ; $i++) {
+				$group_permissions .= $pe[$i];
+				$j = $i+1;
+				if(isset($pe[$j])) { $group_permissions .= ","; } // how about this?
+				}
+				
+				// Now insert into db:
+				$database->Execute("INSERT INTO group_list(id, group_name, group_permissions, group_members) VALUES(?, ? , ?, ?)", array(NULL, $group_name, $group_permissions, NULL));
+				log_info("user_level_editor", "Added User Level: $level_name");
+				
+				$page->set_mode("redirect");
+				$page->set_redirect(make_link("groups/editor"));
+			}
 		}
 	}
 }
---
+++ ext/permissions/theme.php
@@ -0,0 +1,114 @@
+<?
+class GroupEditorTheme extends Themelet {
+	public function display_editor($permissions, $groups) {
+		global $page;
+		$html = $this->get_html_for_groups_editor($permissions, $groups);
+		$page->set_title("Groups Editor");
+		$page->set_heading("Groups Editor");
+                $page->add_block(new Block("Welcome to the Groups Editor!", $html, "main", 10));
+	}
+	
+	private function get_html_for_groups_editor($permissions, $groups) {
+		/**
+		 * Long function name, but at least I won't confuse it with something else ^_^
+		 *
+		 * I'm not great with interfaces unless I'm working in Dreamweaver (drat) so I'll have to copy from the alias editor.
+		 * I'll also have to copy from myself, as this is a direct copy of the user level editor almost.
+		 */
+
+		$html = "";
+			$table_header =  "<tr>
+						<th>ID</th>
+						<th>Group Name</th>";
+			$add_new = "
+				<tr><td>&nbsp;</td>
+					<form action='".make_link("groups/add")."' method='POST'>
+						<td><input style='text-align:center;' type='text' name='group_name' /></td>
+						";
+			// Put all custom permissions here
+			for ($i = 0 ; $i < count($permissions) ; $i++)
+			{
+				/**
+				 * Remember: $permissions[$i]['name']
+				 * Also remember: isset()? "Y" : "N" ;
+				 */
+				$table_header .= "<th>{$permissions[$i]['perm_desc']}</th>";
+				$add_new .= "<td><input type='checkbox' name='{$permissions[$i]['perm_name']}' /></td>";
+			}
+			// Finish the table generation.
+			$table_header .= "<th>Action</th>
+							 </tr>";
+			$add_new .= "<td><input type='submit' value='Add'></td>
+						</form>
+						</tr>";
+		// Now, time for table rows.
+		$table_rows = "";
+        for ($i = 0 ; $i < count($groups) ; $i++)
+        {
+		/**
+		 * Add table rows
+		 */
+			$id = $groups[$i]['id'];
+			$group_name = $groups[$i]['group_name'];
+			
+			$table_rows	 .=  "<tr>
+							<td>$id</td>
+							<td>$group_name</td>";
+							
+			/**
+			 * How do we figure out if all these permissions are set?
+			 * Explode... something.
+			 */
+			$gp = explode(",", $groups[$i]['group_permissions']);
+			//echo count($gp);
+			for ($j = 0 ; $j < count($permissions) ; $j++)
+			{
+				/**
+				 * Remember: $permissions[$i]['perm_name']
+				 * Also remember: isset()? "Y" : "N" ;
+				 */
+			
+			$yn = "N";
+			
+			for ($k = 0 ; $k < count($permissions) ; $k++) 
+			{
+				if(isset($gp[$k])) {
+				
+				if($gp[$k]==$permissions[$j]['perm_name']) { 
+				
+				$yn = "Y";}
+				}
+			}
+			if($yn == "Y") { 
+			$table_rows .= "<td>Y</td>";
+			} else { $yn = "N"; $table_rows .= "<td>N</td>"; }
+			
+			}
+
+			
+							
+			$table_rows  .=		"<td>
+								<form action='".make_link("user_levels/remove")."' method='POST'>
+									<input type='hidden' name='id' value='$id'>
+									<input type='hidden' name='group_name' value='$group_name'>
+									<input type='submit' value='Remove'>
+								</form>
+							</td>
+							</tr>";
+		}
+
+		$html = "
+			<table id='user_levels' class='zebra'>
+				<thead>$table_header</thead>
+				<tbody>$table_rows</tbody>
+				<tfoot>$add_new</tfoot>
+			</table>
+			
+			<br />
+			<b>Help:</b><br />
+			<blockquote>Set <i>group permissions</i> for each group. You can assign members to these groups... later.</blockquote>";
+		
+		return $html;
+	}
+}
+?>
