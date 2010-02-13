5fc87b40dd06f6bf9e3ab377c47b96877908cf5c
Zach Hall <zach@sosguy.net>
2010-02-12 16:54:17 -0800

    Redesigned the group editor to look like Shish's mockup.
    Still need to get actual group editing done.

--- ext/permissions/theme.php
+++ ext/permissions/theme.php
@@ -8,40 +8,49 @@ class GroupEditorTheme extends Themelet {
                 $page->add_block(new Block("Welcome to the Groups Editor!", $html, "main", 10));
 	}
 	
+	private function is_odd($number) {
+	   return $number & 1; // 0 = even, 1 = odd
+	}
+	
 	private function get_html_for_groups_editor($permissions, $groups) {
 		/**
 		 * Long function name, but at least I won't confuse it with something else ^_^
 		 *
-		 * I'm not great with interfaces unless I'm working in Dreamweaver (drat) so I'll have to copy from the alias editor.
-		 * I'll also have to copy from myself, as this is a direct copy of the user level editor almost.
+		 * Editing this a bit to use a new table for each group. I think I see what Shish means
+		 * when he says that I need a "permission_exists" thing.
 		 */
 
 		$html = "";
-			$table_header =  "<tr>
-						<th>Group Name</th>";
-			$add_new = "
-				<tr>
-					<form action='".make_link("groups/add")."' method='POST'>
-						<td><input style='text-align:center;' type='text' name='group_name' /></td>
-						";
+			// Add_new stuff goes here.
+			$add_new_header = "<tr>
+			<td colspan='4'>Group Name: <input style='text-align:center;' type='text' name='group_name' /></td>
+			</tr>";
+			$add_new = "";
+			$oetd = "";
 			// Put all custom permissions here
 			for ($i = 0 ; $i < count($permissions) ; $i++)
-			{
-				/**
-				 * Remember: $permissions[$i]['name']
-				 * Also remember: isset()? "Y" : "N" ;
-				 */
-				$table_header .= "<th>{$permissions[$i]['perm_desc']}</th>";
-				$add_new .= "<td><input type='checkbox' name='{$permissions[$i]['perm_name']}' /></td>";
+			{				
+				// Show two permissions per TR:
+				$tr_top = "";
+				$tr_bot = "";
+				$td_class = "";
+				if(!$this->is_odd($i)) {$tr_top = "<tr>";}
+				if($this->is_odd($i)) {$tr_bot = "</tr>"; $oetd = $oetd + 1;}
+				if($this->is_odd($oetd)) { $td_class = "odd"; } else { $td_class = "even"; }
+				// Add the new table row(s)
+				$add_new .= "
+						{$tr_top}
+						<td class='{$td_class}'>{$permissions[$i]['perm_desc']}</td>
+						<td class='{$td_class}'><input type='checkbox' name='{$permissions[$i]['perm_name']}' /></td>
+						{$tr_bot}";
 			}
 			// Finish the table generation.
-			$table_header .= "<th>Action</th>
-							 </tr>";
-			$add_new .= "<td><input type='submit' value='Add'></td>
-						</form>
-						</tr>";
-		// Now, time for table rows.
-		$table_rows = "";
+
+			$add_new_footer = "<input type='submit' class='group_edit_submit' value='Add'>";
+
+
+		// Now, time for group tables.
+		$group_list = "";
         for ($i = 0 ; $i < count($groups) ; $i++)
         {
 		/**
@@ -50,57 +59,81 @@ class GroupEditorTheme extends Themelet {
 			$id = $groups[$i]['id'];
 			$group_name = $groups[$i]['group_name'];
 			
-			$table_rows	 .=  "<tr>
-							<td>$group_name</td>";
+			// Start the table
+			$group_list .= "<table id='$group_name' class='zebra'><thead>";
+			// Now add the group name
+			$group_list	.=  "<tr><td colspan='4'><span style='font-size:110%; font-weight:bold;'>$group_name</span></td></tr></thead><tbody>
+			<form action='".make_link("groups/edit")."' method='POST'>";
 							
 			/**
 			 * How do we figure out if all these permissions are set?
 			 * Explode... something.
 			 */
 			$gp = explode(",", $groups[$i]['group_permissions']);
-			//echo count($gp);
+			
+			$oetd = "";
+			
 			for ($j = 0 ; $j < count($permissions) ; $j++)
 			{
 				/**
 				 * Remember: $permissions[$i]['perm_name']
 				 * Also remember: isset()? "Y" : "N" ;
 				 */
-			
-			$yn = "N";
-			
-			for ($k = 0 ; $k < count($permissions) ; $k++) 
-			{
-				if(isset($gp[$k])) {
 				
-				if($gp[$k]==$permissions[$j]['perm_name']) { 
+				// Reset vars:
+				$yn = "";
+				$tr_top = "";
+				$tr_bot = "";
+				$td_class = "";
 				
-				$yn = "Y";}
+				for ($k = 0 ; $k < count($permissions) ; $k++) {
+					if(isset($gp[$k])) {
+						if($gp[$k]==$permissions[$j]['perm_name']) { 
+							$yn = "checked";
+						}
+					}
 				}
+				
+				if(!$this->is_odd($j)) {$tr_top = "<tr>";}
+				if($this->is_odd($j)) {$tr_bot = "</tr>"; $oetd = $oetd + 1;}
+				if($this->is_odd($oetd)) { $td_class = "odd"; } else { $td_class = "even"; }
+				// Add the new table row(s)
+				$group_list .= 
+					"{$tr_top}
+						<td class='{$td_class}' width='45%'>{$permissions[$j]['perm_desc']}</td>
+						<td class='{$td_class}' width='5%'><input type='checkbox' name='{$permissions[$j]['perm_name']}' $yn /></td>
+					{$tr_bot}";
 			}
-			if($yn == "Y") { 
-			$table_rows .= "<td>Y</td>";
-			} else { $yn = "N"; $table_rows .= "<td>N</td>"; }
+			$group_list .= "</tbody><tfoot><tr>";
+			$group_list .= "<td colspan='2'><input type='submit' value='Edit'></td></form>";
 			
-			}
-
 			
-							
-			$table_rows  .=		"<td>
-								<form action='".make_link("groups/remove")."' method='POST'>
-									<input type='hidden' name='id' value='$id'>
-									<input type='hidden' name='group_name' value='$group_name'>
-									<input type='submit' value='Remove'>
-								</form>
-							</td>
-							</tr>";
+			if($group_name != "anonymous" && $group_name != "user" && $group_name != "admin") {
+				$group_list  .=		"<td colspan='2'>
+									<form action='".make_link("groups/remove")."' method='POST'>
+										<input type='hidden' name='id' value='$id'>
+										<input type='hidden' name='group_name' value='$group_name'>
+										<input type='submit' style='width:100%;' value='Remove'>
+									</form></td>";
+			} else { $group_list .= "<td colspan='2'><i>System Required Group</i></td>"; }
+			$group_list .= "</tr></tfoot></table><br />";
+			
 		}
 
-		$html = "
-			<table id='user_levels' class='zebra'>
-				<thead>$table_header</thead>
-				<tbody>$table_rows</tbody>
-				<tfoot>$add_new</tfoot>
+		$html = "<b>Current Groups:</b>
+			$group_list
+		
+			<br /><b>Add New Group:</b><br />
+			<form action='".make_link("groups/add")."' method='POST'>
+			<table id='add_new' class='zebra'>
+				<thead>$add_new_header</thead>
+				<tbody>$add_new</tbody>
+				<tfoot><tr><td colspan='4'>$add_new_footer</td></tr></tfoot>
 			</table>
+			
+			</form>
+			
+			
 			
 			<br />
 			<b>Help:</b><br />
