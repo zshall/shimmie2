672db7d44489f32e2df9db65162a29ba79884a9b
Zach Hall <zach@sosguy.net>
2010-02-21 00:50:49 -0800

    Milestone 1. Not ready until the permissions and preferences systems are done.

--- contrib/simple_blog/main.php
+++ contrib/simple_blog/main.php
@@ -32,6 +32,32 @@ class SimpleBlogPermissions extends SimpleExtension {
 	}
 }
 
+class SimpleBlogConfig extends SimpleExtension {
+    /**
+     * Don't know why, but I love keeping config seperate from the rest.
+     */
+    public function onSetupBuilding($event) {
+        $sb = new SetupBlock("Simple Blog");
+        $sb->add_text_option("blog_title", "Blog Title: <br>");
+        // These settings inspired by Bzchan's fabulous /home/ extension. Thanks!
+        $sb->add_longtext_option("blog_sidebar", '<br>Sidebar Links - Example: [/post/list|Posts]<br>');
+        $sb->add_longtext_option("blog_header", "<br>Custom Header:<br>");
+        $sb->add_int_option("blog_posts_per_page", "<br>Blog posts per page: <br>");
+        $event->panel->add_block($sb);
+    }
+    public function onInitExt(Event $event) {
+        global $config;
+        $sitename = $config->get_string("title");
+        $config->set_default_string("blog_title", "$sitename Blog");
+        $config->set_default_string("blog_sidebar", "[/post/list|Posts]<br>[/comment/list|Comments]");
+        $config->set_default_string("blog_header", "Featuring the latest updates and news from $sitename!");
+        $config->set_default_int("blog_posts_per_page", 5);
+    }
+}
+
+// Null Events:
+class BlogBuildingEvent extends Event {}
+
 class SimpleBlog extends SimpleExtension {
     /**
      * My simple blog extends a simple extension... how fitting.
@@ -74,7 +100,7 @@ class SimpleBlog extends SimpleExtension {
     public function onUserBlockBuilding(Event $event) {
         global $user;
         if($user->is_admin()) {
-                $event->add_link("Blog Manager", make_link("blog/editor"));
+                $event->add_link("Blog Manager", make_link("blog_manager"));
         }
     }
     public function addPost($userid, $title, $text) {
@@ -87,11 +113,11 @@ class SimpleBlog extends SimpleExtension {
         log_info("simple_blog", "Added Message: $title");
     }
     public function onPageRequest(Event $event) {
-        if($event->page_matches("blog")) {
+        if($event->page_matches("blog_manager")) {
             switch($event->get_arg(0)) {
-                case "editor":		
+                case "":		
                     /**
-                     * Displays the blog editor.
+                     * Displays the blog manager.
                      */
                     global $page, $database, $user;
                     if(!$user->can("manage_blog")) {
@@ -116,7 +142,7 @@ class SimpleBlog extends SimpleExtension {
                             // Now insert into db:
                             $this->addPost($user->id, $post_title, $post_text);
                             $page->set_mode("redirect");
-                            $page->set_redirect(make_link("blog/editor"));
+                            $page->set_redirect(make_link("blog_manager"));
                     }
                     break;
                 case "remove":
@@ -132,23 +158,156 @@ class SimpleBlog extends SimpleExtension {
                             $database->Execute("DELETE FROM blotter WHERE id=?", array($id));
                             log_info("simple_blog", "Removed Entry #$id");
                             $page->set_mode("redirect");
-                            $page->set_redirect(make_link("blog/editor"));
-                    }
-                    break;
-                case "":
-                    /**
-                     * Displays the blog
-                     */
-                    global $database, $user;
-                    if(!$user->can("view_blog")) {
-                        $this->theme->display_permisssion_denied($page);
-                    } else {
-                        $posts = $database->get_all("SELECT * FROM simple_blog ORDER BY id DESC");
-                        $this->theme->display_blog_index($posts);
+                            $page->set_redirect(make_link("blog_manager"));
                     }
                     break;
             }
         }
+        if($event->page_matches("blog")) {
+            global $database, $user, $config, $page;
+            /**
+             * Displays the blog
+             */
+            if(!$user->can("view_blog")) {
+                $this->theme->display_permission_denied($page);
+            } else {
+                switch ($event->get_arg(0)) {
+                    case "":
+                    case "list":
+                        /**
+                         * Pagination always helps.
+                         */
+                        if(is_null($event->get_arg(1))||$event->get_arg(1)<=0) {
+                            $current_page = 1;
+                        } else {
+                            $current_page = $event->get_arg(1);
+                        }
+                        
+                        $posts_per_page = $config->get_int("blog_posts_per_page");
+                        $start = $posts_per_page * ($current_page - 1);
+                        
+                        $posts = $database->get_all("SELECT *
+                                                    FROM simple_blog
+                                                    ORDER BY id DESC
+                                                    LIMIT ? OFFSET ?",
+                                                    array($posts_per_page, $start));
+                        
+                        $total_pages = (int)($database->db->GetOne("SELECT COUNT(*) FROM simple_blog") / $posts_per_page);
+            
+                        $this->theme->display_blog_index($posts, $current_page, $total_pages);
+                        break;
+                    case "view":
+                        global $database, $config;
+                        if(is_null($event->get_arg(1))||$event->get_arg(1)<=0) {
+                            $current_post = 1;
+                        } else {
+                            $current_post = $event->get_arg(1);
+                        }
+                        
+                        $post = $database->get_row("SELECT *
+                                                   FROM simple_blog
+                                                   ORDER BY id DESC");
+                        
+                        $this->theme->display_blog_post($post);
+                        break;
+                }
+            }
+        }
     }
+}
+
+class SimpleBlogRSS extends SimpleExtension {
+    /**
+     * Adapted from Shish's Image RSS extension.
+     */
+	public function onBlogBuilding($event) {
+		global $config, $page;
+		$title = $config->get_string('blog_title');
+		$page->add_header("<link id=\"posts\" rel=\"alternate\" type=\"application/rss+xml\" ".
+                    "title=\"$title\" href=\"".make_link("rss/blog/1")."\" />");
+	}
+
+	public function onPageRequest($event) {
+            global $config, $database;
+            if($event->page_matches("rss/blog")) {
+                if($user->can("view_blog")) {
+                    if(is_null($event->get_arg(0))||$event->get_arg(0)<=0) {
+                        $current_page = 1;
+                    } else {
+                        $current_page = $event->get_arg(0);
+                    }
+                    
+                    $posts_per_page = $config->get_int("blog_posts_per_page");
+                    $start = $posts_per_page * ($current_page - 1);
+                    
+                    $posts = $database->get_all("SELECT *
+                                                FROM simple_blog
+                                                ORDER BY id DESC
+                                                LIMIT ? OFFSET ?",
+                                                array($posts_per_page, $start));
+                    
+                    $this->do_rss($posts, $current_page);
+                }
+            }
+	}
+
+
+	private function do_rss($posts, $page_number) {
+	    global $page;
+            global $config;
+            $page->set_mode("data");
+            $page->set_type("application/rss+xml");
+
+            $data = "";
+            for ($i = 0 ; $i < count($posts) ; $i++)
+            {
+
+                    $link = make_http(make_link("blog/view/{$posts[$i]['id']}"));
+                    $owner = User::by_id($posts[$i]['owner_id']);
+                    $posted = date(DATE_RSS, strtotime($posts[$i]['post_date']));
+                    $content = html_escape(
+                            "<p>" . $posts[$i]['post_text'] . "</p>" .
+                            "<p>Posted by " . $owner->name . "</p>"
+                    );
+
+                    $data .= "
+            <item>
+                    <title>{$posts[$i]['post_title']}</title>
+                    <link>$link</link>
+                    <guid isPermaLink=\"true\">$link</guid>
+                    <pubDate>$posted</pubDate>
+                    <description>$content</description>
+            </item>
+                    ";
+            }
+
+            $title = $config->get_string('title');
+            $base_href = make_http($config->get_string('base_href'));
+
+            if($page_number > 1) {
+                    $prev_url = make_link("rss/blog/".($page_number-1));
+                    $prev_link = "<atom:link rel=\"previous\" href=\"$prev_url\" />";
+            }
+            else {
+                    $prev_link = "";
+            }
+            $next_url = make_link("rss/blog/".($page_number+1));
+            $next_link = "<atom:link rel=\"next\" href=\"$next_url\" />"; // no end...
+
+            $version = VERSION;
+            $xml = "<"."?xml version=\"1.0\" encoding=\"utf-8\" ?".">
+<rss version=\"2.0\" xmlns:media=\"http://search.yahoo.com/mrss\" xmlns:atom=\"http://www.w3.org/2005/Atom\">
+    <channel>
+        <title>$title</title>
+        <description>Latest Blog Posts</description>
+		<link>$base_href</link>
+		<generator>Shimmie-$version</generator>
+		$prev_link
+		$next_link
+		$data
+	</channel>
+</rss>";
+		$page->set_data($xml);
+	}
 }
 ?>
--- contrib/simple_blog/theme.php
+++ contrib/simple_blog/theme.php
@@ -8,14 +8,55 @@ class SimpleBlogTheme extends Themelet {
         $page->add_block(new Block("Welcome to the Blog Manager!", $html, "main", 10));
         $page->add_block(new Block("Navigation", "<a href='".make_link()."'>Index</a>", "left", 0));
     }
-    public function display_blog_index($posts) {
+    public function display_blog_index($posts, $page_number, $total_pages) {
         global $page, $config;
-        $sitename = $config->get_string('title');
+        /**
+         * Pagination:
+         */
+        $prev = $page_number - 1;
+	$next = $page_number + 1;
         
-        $page->set_title("$sitename Blog");
-        $page->set_heading("$sitename Blog");
+        $h_prev = ($page_number <= 1) ? "Prev" :
+                "<a href='".make_link("blog/$prev")."'>Prev</a>";
+        $h_index = "<a href='".make_link("blog/list")."'>Index</a>";
+        $h_home = "<a href='".make_link()."'>&#171; Home</a>";
+        $h_next = ($page_number >= $total_pages) ? "Next" :
+                "<a href='".make_link("blog/$next")."'>Next</a>";
+
+	$nav = "$h_prev | $h_index | $h_next<br />$h_home";
+        /**
+         * Displaying the blog:
+         */
+        
+        $title = $config->get_string('blog_title');
+        
+        $page->set_title($title);
+        $page->set_heading($title);
+        $this->generate_blog_header($nav);
         $this->generate_blog_index($posts);
-        $page->add_block(new Block("Navigation", "<a href='".make_link()."'>Index</a>", "left", 0));
+        $this->display_paginator($page, "blog/list", null, $page_number, $total_pages);
+        send_event(new BlogBuildingEvent()); // make it extendable.
+    }
+    public function display_blog_post($post) {
+        global $page, $config;
+        /**
+         * Displaying the blog:
+         */
+        
+        $title = $config->get_string('blog_title');
+        
+        $page->set_title($title);
+        $page->set_heading($title);
+        $this->generate_blog_header("<a href='".make_link("blog/list")."'>Index</a><br /><a href='".make_link()."'>&#171; Home</a>");
+        
+        $this->generate_blog_post($post['id'],
+                                 $post['owner_id'],
+                                 $post['post_date'],
+                                 $post['post_title'],
+                                 $post['post_text'],
+                                 0);
+        
+        send_event(new BlogBuildingEvent()); // make it extendable.
     }
     private function is_odd($number) {
             return $number & 1; // 0 = even, 1 = odd
@@ -34,7 +75,7 @@ class SimpleBlogTheme extends Themelet {
             <th>Action</th>
             </tr>";
         $add_new = "
-            <form action='".make_link("blog/add")."' method='POST'>
+            <form action='".make_link("blog_manager/add")."' method='POST'>
             <table class='zebra'>
             <tr class='odd'><td style='width: 30px;'>Title</td><td><input type='text' name='post_title' maxlength='120' /></td></tr>
             <tr class='even'>
@@ -66,7 +107,7 @@ class SimpleBlogTheme extends Themelet {
                 <td>$post_date</td>
                 <td>$post_title</td>
 
-                <td><form name='remove$id' method='post' action='".make_link("blog/remove")."'>
+                <td><form name='remove$id' method='post' action='".make_link("blog_manager/remove")."'>
                 <input type='hidden' name='id' value='$id' />
                 <input type='submit' style='width: 100%;' value='Remove' />
                 </form>
@@ -88,37 +129,102 @@ class SimpleBlogTheme extends Themelet {
 
         return $html;
     }
-    private function generate_blog_index($posts) {
-        global $page;
-        for ($i = 0 ; $i < count($posts) ; $i++)
-        {
-            /**
-             * Add table rows
-             */
-            $id = $posts[$i]['id'];
-            $post_author = User::by_id($posts[$i]['owner_id']);
-            $post_date = $posts[$i]['post_date'];
+    private function generate_blog_post($pi, $pa, $pd, $ph, $pb, $i) {
+            global $page, $config;
+            $id = $pi;
+            $post_author = User::by_id($pa);
+            $post_date = $pd;
             $clean_date = date("m/d/y", strtotime($post_date));
-            $post_title = $posts[$i]['post_title'];
+            $post_title = $ph;
             
-            $post_text = $posts[$i]['post_text'];
-
+            $post_text = $pb;
+            
             $tfe = new TextFormattingEvent($post_text);
             send_event($tfe);
             $post_text = $tfe->formatted;
-
+            
 	    $post_text = str_replace('\n\r', '<br>', $post_text);
             $post_text = str_replace('\r\n', '<br>', $post_text);
             $post_text = str_replace('\n', '<br>', $post_text);
             $post_text = str_replace('\r', '<br>', $post_text);
 	    $post_text = stripslashes($post_text);
-
-            $body = "
-                <h4>Written by {$post_author->name} on $clean_date</h4>
+            
+            $pattern = '/\[([^:]+):([^\]]+)\]/';
+            preg_match_all($pattern, $post_text, $matches);
+            
+            /**
+             * We now have a $matches array:
+             * [0][x] - what to find / replace ([image:25])
+             * [1][x] - the command portion (image)
+             * [2][x] - the parameter portion (25)
+             */
+            
+            for ($j = 0; $j < count($matches); $j++) {
+                if(isset($matches[0][$j])) {
+                    switch ($matches[1][$j]) {
+                        case "image":
+                            if(isset($matches[2][$j])) {
+                                $origtxt = html_escape($matches[0][$j]);
+                                $imageid = int_escape($matches[2][$j]);
+                                $image = Image::by_id($imageid);
+                                $src = $image->get_image_link();
+                                switch($image->ext) {
+                                    case "jpg":
+                                    case "gif":
+                                    case "png":
+                                        $to_replace = "<a href='".make_link("post/view/{$image->id}")."'><img id='main-image' src='{$src}' style='max-width:100%;' /></a>";
+                                        $post_text = str_replace($origtxt, $to_replace, $post_text);
+                                        break;
+                                }
+                            }
+                            break;
+                        case "comment":
+                            break;
+                    }
+                }
+            }
+            
+            $body = "<div class='blog-body'>
+                <span class='blog-header'><a href='".make_link("blog/view/$id")."'>#</a> Written by {$post_author->name} on $clean_date<br /><br /></span>
                 $post_text
+                </div>
             ";
-            $page->add_block(new Block($post_title, $body, "main", $i));
+            
+            $page->add_block(new Block($post_title, $body, "main", ($i+5)));
+    }
+    private function generate_blog_index($posts) {
+        global $page, $config;
+        
+        for ($i = 0 ; $i < count($posts) ; $i++)
+        {
+            /**
+             * Show posts
+             */
+            $this->generate_blog_post($posts[$i]['id'],
+                                     $posts[$i]['owner_id'],
+                                     $posts[$i]['post_date'],
+                                     $posts[$i]['post_title'],
+                                     $posts[$i]['post_text'],
+                                     $i);
         }
+        $page->add_block(new Block(NULL,$config->get_string("blog_header"), "main", 0));
+    }
+    private function generate_blog_header($nav=null) {
+        global $page, $config;
+        $page->add_header("<style type'text/css'>
+                        .blog-header {
+                            font-size:80%;
+                        }
+                        .blog-body {
+                            text-align:left;
+                        }</style>");
+        
+        $base_href = $config->get_string('base_href');
+        $sidebar_links = $config->get_string('blog_sidebar');
+        $sidebar_links = str_replace('$base',	$base_href, 	$sidebar_links);
+        $sidebar_links = preg_replace('#\[(.*?)\|(.*?)\]#', "<a href='\\1'>\\2</a>", $sidebar_links);
+        $sidebar_links = str_replace('//',	"/", $sidebar_links);
+        $page->add_block(new Block("Navigation", $nav . "<br />". $sidebar_links, "left", 10));
     }
 }
 ?>
